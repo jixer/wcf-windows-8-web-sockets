7befd9e63c8ee6a9eba756615a56066646464d37
jixer <gsxr.signup@gmail.com>
2012-11-14 09:31:09 -0800

    Added powershell script for removing bin / obj folders and .suo file

---
+++ Cleanup.ps1
@@ -0,0 +1,26 @@
+﻿$workingDir = 'C:\Users\myers_000\Desktop\NovCodeCamp\wcf-windows-8-web-sockets\';
+cd $workingDir;
+$binfolders = Get-ChildItem -Force -Recurse -Filter "bin";
+
+#5. Scan for more folders with this name.  When none more found, stop.
+foreach($FolderName in $binfolders)
+{
+    Remove-Item -Path $FolderName.FullName -Recurse -Force;
+                
+}
+
+$objfolders = Get-ChildItem -Force -Recurse -Filter "obj";
+
+#5. Scan for more folders with this name.  When none more found, stop.
+foreach($FolderName in $objfolders)
+{
+    Remove-Item -Path $FolderName.FullName -Recurse -Force;
+                
+}
+
+$suofiles = Get-ChildItem -Hidden -Filter "*.suo";
+
+foreach($FileName in $suofiles)
+{
+    Remove-Item -Force $FileName.FullName;
+}
